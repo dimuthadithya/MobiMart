a886fb2cac42ff7a7bc5001c921aebc85dd32ab4
dimuthx353 <dimuthx353@gmail.com>
2025-05-08 17:57:40 +0530

    create components

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$db   = 'ministore';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    Optional:
+    echo "Connection successful";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
